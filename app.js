31dc75206deb81389b13be8082a0e4929f23ff28
Nancy Tesfaye <nancy23tes@gmail.com>
2024-08-20 18:40:26 +0300

    update

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use("/api/questions", questionRoute)
 app.use("/api/answers", answerRoute)
 
 // const port = 3333;
-const port = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT || 3000;
 app.use((req, res, next) => {
     res.status(404).json({ msg: 'Resource not found error.' });
 });
